3782b7bacef8e415eb2c9dd9af94b6b5727ccc6b
Nicholas Giggey <nicholasgiggey@Nicholass-iMac.local>
2022-03-02 15:32:38 -0700

    completed

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ function toTop() {
     window.scrollTo(0, 0);
 }
 // --------------------------------------------End---------------------------------------------
+// ----------------------------------------FingerCiser-----------------------------------------
+const calorieCounter = document.querySelector(".treadmill");
+calorieCounter.addEventListener("scroll", scrollTop);
+let count = 0;
+function scrollTop() {
+    count++;
+    document.getElementById("progress").innerText = count;
+};
+// --------------------------------------------End---------------------------------------------
+// -------------------------------------Christmas Lights---------------------------------------
+document.getElementById('lightChanger').addEventListener('click', function () {
+    if (this.classList.contains('off')) {
+        this.classList.remove('off');
+    } else {
+        this.classList.add('off');
+    }
+});
